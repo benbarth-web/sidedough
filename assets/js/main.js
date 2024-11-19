7d8f15a88c81335882ce74ac699d731e370451c8
Ben Barth <benbarth@users.noreply.github.com>
2024-11-19 17:31:45 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,20 +12,20 @@ $(function() {
 			$wrapper = $('#wrapper');
 
 	// Breakpoints.
-		skel.breakpoints({
-			xlarge:	'(max-width: 1680px)',
-			large:	'(max-width: 1280px)',
-			medium:	'(max-width: 980px)',
-			small:	'(max-width: 736px)',
-			xsmall:	'(max-width: 480px)'
-		});
+		// skel.breakpoints({
+		// 	xlarge:	'(max-width: 1680px)',
+		// 	large:	'(max-width: 1280px)',
+		// 	medium:	'(max-width: 980px)',
+		// 	small:	'(max-width: 736px)',
+		// 	xsmall:	'(max-width: 480px)'
+		// });
 
 	// Disable animations/transitions until everything's loaded.
-		$body.addClass('is-loading');
+		// $body.addClass('is-loading');
 
-		$window.on('load', function() {
-			$body.removeClass('is-loading');
-		});
+		// $window.on('load', function() {
+		// 	$body.removeClass('is-loading');
+		// });
 
 	// Poptrox.
 		// $window.on('load', function() {
