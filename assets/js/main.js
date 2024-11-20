e0b620824c88b0e1247db8597697893a51468691
Ben Barth <benbarth@users.noreply.github.com>
2024-11-19 20:02:37 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,14 @@
 	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
 */
 
+var isLoaded = false;
+
+$(window).on('load', function () {
+	isLoaded = true;
+	// Enable now that everything's loaded.
+	$('body').removeClass('is-loading');
+});
+
 $(function () {
 
 	// Vars.
@@ -12,10 +20,6 @@ $(function () {
 		$wrapper = $('#wrapper');
 
 	// Disable animations/transitions until everything's loaded.
-	$body.addClass('is-loading');
-
-	$window.on('load', function() {
-		$body.removeClass('is-loading');
-	});
+	if (isLoaded === false) $body.addClass('is-loading');
 
 });
